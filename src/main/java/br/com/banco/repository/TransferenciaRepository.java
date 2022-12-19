1bbb48f6783903fa9002f5b99902d964a3aeaca6
Israel Douglas <israeldouglas25@gmail.com>
2022-12-19 18:43:52 -0300

    Criando interface para listar por nome ou conta

--- src/main/java/br/com/banco/repository/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repository/TransferenciaRepository.java
@@ -1,9 +1,16 @@
 package br.com.banco.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import br.com.banco.model.Conta;
 import br.com.banco.model.Transferencia;
 
 public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
+
+	List<Transferencia> findTransferenciaByConta(Conta conta);
+
+	List<Transferencia> findTransferenciaByNomeOperadorTransacao(String name);
 
 }
