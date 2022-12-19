7d7c3329048a9b5e6f715b336af56249f095f116
Israel Douglas <israeldouglas25@gmail.com>
2022-12-19 18:46:51 -0300

    Criando os metodos que busca uma lista por nome ou lista por conta

--- src/main/java/br/com/banco/controller/TransferenciaController.java
+++ src/main/java/br/com/banco/controller/TransferenciaController.java
@@ -1,39 +1,45 @@
 package br.com.banco.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.banco.dto.TransferenciaDto;
 import br.com.banco.model.Transferencia;
-import br.com.banco.repository.ContaRepository;
-import br.com.banco.repository.TransferenciaRepository;
+import br.com.banco.service.TransferenciaService;
 
 @RestController
 @RequestMapping("/transferencias")
 public class TransferenciaController {
 	
 	@Autowired
-	private TransferenciaRepository transferenciaRepository;
-
-	@Autowired
-	private ContaRepository contaRepository;
+	private TransferenciaService transferenciaService;
 	
 	@GetMapping
 	public ResponseEntity<List<Transferencia>> findAll(){
-		List<Transferencia> transferencias = transferenciaRepository.findAll();
+		List<Transferencia> transferencias = transferenciaService.findAll();
 		return ResponseEntity.ok(transferencias);
 	}
 
-	@GetMapping("/{id}")
-	public ResponseEntity<Transferencia> findAllByName(@PathVariable Long id){
-		Optional<Transferencia> transferencia = transferenciaRepository.findById(id);
-		return ResponseEntity.ok(transferencia.get());
+	@GetMapping("/nome")
+	public ResponseEntity<List<Transferencia>> findAllByName(@RequestParam String nome){
+		List<Transferencia> transferencia = transferenciaService.findAllByNome(nome);
+		return ResponseEntity.ok(transferencia);
+	}
+	
+	@GetMapping("/conta/{id}")
+	public ResponseEntity<List<TransferenciaDto>> findAllByConta(@PathVariable Long id){
+		List<Transferencia> transferencia = transferenciaService.findAllByConta(id);
+		List<TransferenciaDto> transferenciaDto = transferencia.stream()
+				.map(TransferenciaDto::of)
+				.toList();
+		return ResponseEntity.ok(transferenciaDto);
 	}
 
 }
